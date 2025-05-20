905597a6d6ba7c8f7511d1ec9ecdcb20090a1e69
ecstrim <1094666+ecstrim@users.noreply.github.com>
2025-05-20 11:23:34 +0200

    Update 00-ingress.sh

--- 00-ingress.sh
+++ 00-ingress.sh
@@ -7,5 +7,8 @@ helm install ingress-nginx ingress-nginx/ingress-nginx \
   --set controller.service.type=LoadBalancer \
   --set controller.service.loadBalancerIP="10.240.0.50" \
   --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"="true" \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal-subnet"="aks-subnet-name"
-
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal-subnet"="aks-subnet-name" \
+  --set controller.nodeSelector."agentpool"="userpool" \
+  --set controller.tolerations[0].key="node-role.kubernetes.io/system" \
+  --set controller.tolerations[0].operator="Exists" \
+  --set controller.tolerations[0].effect="NoSchedule"
